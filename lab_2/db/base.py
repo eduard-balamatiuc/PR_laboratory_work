adf966b5ddf5597e598fe1f8a9f22c4644e67dfa
eduard-balamatiuc <balamatiuc2@gmail.com>
2024-12-05 19:23:50 +0200

    create the base loading db

--- lab_2/db/base.py
+++ lab_2/db/base.py
@@ -1,3 +1,13 @@
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+import os
 
+load_dotenv()
+
+DATABASE_URL = os.getenv("POSTGRES_URL")
+
+engine = create_engine(DATABASE_URL)
+session_local = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
