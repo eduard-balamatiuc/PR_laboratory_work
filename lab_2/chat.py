6f438817f1e8f78075b8f37b6ba82787f2a2ff33
eduard-balamatiuc <balamatiuc2@gmail.com>
2024-12-09 12:32:58 +0200

    remove other random comments

--- lab_2/chat.py
+++ lab_2/chat.py
@@ -6,7 +6,7 @@ import asyncio
 class ChatRoom:
     def __init__(self):
         self.rooms: Dict[str, Set[WebSocket]] = {}
-        self.message_history: Dict[str, List[str]] = {}  # Store message history per room
+        self.message_history: Dict[str, List[str]] = {}
 
     async def join_room(self, room_id: str, websocket: WebSocket):
         await websocket.accept()
@@ -15,7 +15,6 @@ class ChatRoom:
             self.message_history[room_id] = []
         self.rooms[room_id].add(websocket)
         
-        # Send message history to new client
         for message in self.message_history[room_id]:
             await websocket.send_text(message)
 
@@ -28,9 +27,7 @@ class ChatRoom:
 
     async def broadcast_message(self, room_id: str, message: str, sender: WebSocket):
         if room_id in self.rooms:
-            # Store message in history
             self.message_history[room_id].append(message)
-            # Limit history to last 50 messages
             self.message_history[room_id] = self.message_history[room_id][-50:]
             
             for client in self.rooms[room_id]:
